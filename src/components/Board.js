79219385ecf30ff465469d05bdd831d33cf92ab9
ptrkfrsk <ptrqfrsq@gmail.com>
2020-04-01 11:42:06 +0200

    O and X inserting. States overhauled. Now the game is handled from board

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,25 +4,53 @@ import Field from './Field'
 class BoardComponent extends React.Component {
   constructor() {
     super();
+    this.player = "O";
+    this.state = {
+        values: ['','','','','','','','',''],
+        player: 'O',
+    };
+
+  }
+
+  insertValue(i) {
+    const newValues = this.state.values.slice();
+    switch (this.state.player) {
+      case 'O':
+        newValues[i] = 'O';
+        this.setState({
+          player: 'X',
+          values: newValues,
+        });
+        break;
+      case 'X':
+        newValues[i] = 'X';
+        this.setState({
+          player: 'O',
+          values: newValues,
+        });
+        break;
+      default:
+        break;
+    }
   }
 
   render() {
     return (
       <div className="board">
         <div className="fieldRow">
-          <Field />
-          <Field />
-          <Field />
+          <Field value={this.state.values[0]} onClick={() => { this.insertValue(0);} }/>
+          <Field value={this.state.values[1]} onClick={() => { this.insertValue(1);} }/>
+          <Field value={this.state.values[2]} onClick={() => { this.insertValue(2);} }/>
         </div>
         <div className="fieldRow">
-          <Field />
-          <Field />
-          <Field />
+          <Field value={this.state.values[3]} onClick={() => { this.insertValue(3);} }/>
+          <Field value={this.state.values[4]} onClick={() => { this.insertValue(4);} }/>
+          <Field value={this.state.values[5]} onClick={() => { this.insertValue(5);} }/>
         </div>
         <div className="fieldRow">
-          <Field />
-          <Field />
-          <Field />
+          <Field value={this.state.values[6]} onClick={() => { this.insertValue(6);} }/>
+          <Field value={this.state.values[7]} onClick={() => { this.insertValue(7);} }/>
+          <Field value={this.state.values[8]} onClick={() => { this.insertValue(8);} }/>
         </div>
         <button onClick={()=>alert("Reset")}>Reset</button>
       </div>
--- src/components/Field.js
+++ src/components/Field.js
@@ -1,32 +1,17 @@
 import React from 'react';
-let circle = true;
 
 class FieldComponent extends React.Component {
-  constructor() {
-    super();
-    this.state = { value: "" };
-  }
-
-  insertValue() {
-    if (this.state.value === "") {
-      if (circle === true) {
-        this.setState({value: "O"});
-        circle = false;
-      } else {
-        this.setState({value: "X"});
-        circle = true;
-      }
-    }
-  }
-
-  resetValue() {
-    this.setState({ value: "" } );
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+    };
   }
 
   render() {
     return (
-      <div className="field" onClick={ this.insertValue.bind(this) }>
-        { this.state.value }
+      <div className="field" onClick={this.props.onClick}>
+        { this.props.value }
       </div>
     );
   }
